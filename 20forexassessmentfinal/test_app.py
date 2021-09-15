cdd8a3377596e85933093f2911f8abaa58ab31dc
Alex Jones <geffalid7@yahoo.com>
2021-09-14 20:48:47 -0700

    change in textapp forex

--- 20forexassessmentfinal/test_app.py
+++ 20forexassessmentfinal/test_app.py
@@ -15,4 +15,34 @@ class AddTest(TestCase):
             res = client.post('/result', data={'amount': '€84.41668073611345'})
             html = res.get_data(as_text=True)
 
-            self.assertIn('<h1>The result is €84.41668073611345</h1>')
+            self.assertIn('<h1>The result is €84.41668073611345</h1>', html)
+
+    def test_result100eurotousd(self):
+        with app.test_client() as client:
+            res = client.post('/result', data={'amount': 'US$118.6'})
+            html = res.get_data(as_text=True)
+
+            self.assertIn('<h1>The result is US$118.6</h1>', html)
+
+    def test_flash_message1(self):
+        with app.test_client() as client:
+            res = client.post(
+                '/result', data={'convertfr': 'Not a valid code'})
+            html = res.get_data(as_text=True)
+
+            self.assertIn('<div>Not a valid code</div>', html)
+
+    def test_flash_message2(self):
+        with app.test_client() as client:
+            res = client.post(
+                '/result', data={'convertto': 'Not a valid code'})
+            html = res.get_data(as_text=True)
+
+            self.assertIn('<div>Not a valid code</div>', html)
+
+    def test_flash_message3(self):
+        with app.test_client() as client:
+            res = client.post('/result', data={'amount': 'Not a valid amount'})
+            html = res.get_data(as_text=True)
+
+            self.assertIn('<div>Not a valid amount</div>', html)
